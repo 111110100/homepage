1d88970becdf5086b458e2fc833f141dd32dcda4
111110100 <elomibao@gmail.com>
2021-05-13 11:07:00 +0800

    Added checks if window already open centered windows adjusted width, height, top, bottom

--- public/js/main.js
+++ public/js/main.js
@@ -3,16 +3,28 @@ const contact = document.querySelector('#contact')
 const aboutContent = document.querySelector('#about-content')
 const contactContent = document.querySelector('#contact-content')
 
+const horizontalCenter = Math.floor((window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth)/2);
+const verticalCenter = Math.floor((window.innerHeight|| document.documentElement.clientHeight.document.body.clientHeight)/2);
+
+let aboutOpen = false
+let contactOpen = false
+
 about.addEventListener('click', () => {
+  if (aboutOpen) {
+    return
+  }
+  aboutOpen = true
   const aboutBox = new WinBox({
     title: 'About Me',
     // modal: true,
+    x: "center",
+    y: "center",
     width: '400px',
     height: '400px',
-    top: 50,
-    right: 50,
-    bottom: 50,
-    left: 50,
+    top: 5,
+    right: 5,
+    bottom: 5,
+    left: 5,
     mount: aboutContent,
     onfocus: function () {
       this.setBackground('#00aa00')
@@ -20,19 +32,29 @@ about.addEventListener('click', () => {
     onblur: function () {
       this.setBackground('#777')
     },
+    onclose: function() {
+      aboutOpen = false
+      return
+    }
   })
 })
 
 contact.addEventListener('click', () => {
+  if (contactOpen) {
+    return
+  }
+  contactOpen = true
   const contactBox = new WinBox({
     title: 'Contact Me',
+    x: horizontalCenter - 150,
+    y: verticalCenter - 150,
     background: '#00aa00',
     width: '400px',
     height: '400px',
-    top: 150,
-    right: 50,
-    bottom: 50,
-    left: 250,
+    top: 5,
+    right: 5,
+    bottom: 5,
+    left: 5,
     mount: contactContent,
     onfocus: function () {
       this.setBackground('#00aa00')
@@ -40,5 +62,9 @@ contact.addEventListener('click', () => {
     onblur: function () {
       this.setBackground('#777')
     },
+    onclose: function() {
+      contactOpen = false
+      return
+    }
   })
 })
